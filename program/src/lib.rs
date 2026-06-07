0a928cc4f2fcaac5759c8f496b2236cb06f4c0ef
Singupalli Kartik <skartik1706@gmail.com>
2026-06-08 00:14:00 +0530

    init project repo with create_idempotent

---
+++ program/src/lib.rs
@@ -0,0 +1,45 @@
+use pinocchio::{
+    AccountView, Address, ProgramResult,
+    entrypoint,
+    error::ProgramError,
+
+};
+
+mod batch;
+mod create_idempotent;
+
+
+use crate::create_idempotent::process_create_idempotent_instruction;
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Address,
+    accounts: &mut [AccountView],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    match AssociatedTokenAccountInstruction::try_from_bytes(instruction_data)? {
+        AssociatedTokenAccountInstruction::Create => todo!(),
+        AssociatedTokenAccountInstruction::CreateIdempotent => process_create_idempotent_instruction(program_id, accounts, instruction_data),
+        AssociatedTokenAccountInstruction::RecoverNested => todo!(),
+    }
+}
+
+
+enum AssociatedTokenAccountInstruction {
+    Create,
+    CreateIdempotent,
+    RecoverNested,
+}
+
+
+impl AssociatedTokenAccountInstruction {
+    pub fn try_from_bytes(instruction_data: &[u8]) -> Result<Self, ProgramError> {
+        match instruction_data {
+            [] | [0] => Ok(Self::Create),
+            [1] => Ok(Self::CreateIdempotent),
+            [2] => Ok(Self::RecoverNested),
+            _ => Err(ProgramError::InvalidInstructionData),
+        }
+    }
+}
